e8dd2765edb680550ba5ebce1295e025f430410a
Alexander <kaamosdao@gmail.com>
2023-09-13 17:14:02 +0300

    Change shaders: add distortion on mousemove

--- app/_shaders/fragment.glsl
+++ app/_shaders/fragment.glsl
@@ -3,11 +3,32 @@ precision mediump float;
 #endif
 
 uniform sampler2D image;
+uniform vec2 resolution;
+uniform float time;
+uniform vec2 mouse;
 
 varying vec2 vUv;
 
 void main() {
-  vec4 color = texture2D(image, vUv);
+  vec2 p = (7.68 * gl_FragCoord.xy / resolution.xy) - vec2(0.5, 1.0);
+  vec2 i = p;
+  float c = 1.0;
+
+  float inten = 1.1;
+
+  for(int n = 0; n < 4; n++){
+    float t = time * (1.0 - (10.0 / float(n + 10))) * 1.5;
+    float ix = i.x + mouse.x;
+    float iy = i.y + mouse.y;
+
+    i = p + vec2(cos(t - ix) + sin(t + iy), sin(t - iy) + cos(t + ix));
+		c += float(n)/length(vec2(p.x/sin(ix + t)/inten, p.y/cos(iy + t)/inten)) * 20.;
+  }
+
+  c /= 90.;
+  c = 1.8 - sqrt(c);
+
+  vec4 color = texture2D(image, vUv) * texture2D(image, vec2(vUv.x + cos(c) * mouse.x * 0.05, vUv.y + sin(c) * mouse.y * 0.05 )) * 0.75;
 
   gl_FragColor = color;
 }
--- app/_shaders/vertex.glsl
+++ app/_shaders/vertex.glsl
@@ -16,7 +16,7 @@ void main() {
   vUv *= uvRate.xy;
   vUv += 0.5;
 
-  float vWave = sin(time + (position.x + position.y) * waveLength) * 0.04;
+  float vWave = sin(time + (position.x + position.y) * waveLength) * 0.02;
 
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position.x + mouse.y * 0.02, position.y + mouse.x * 0.02, vWave, 1.0);
 }
