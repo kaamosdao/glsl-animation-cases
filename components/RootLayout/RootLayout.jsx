a3e7c522aee681b2ba6f70f1b35b99b278d0f599
Alexander <kaamosdao@gmail.com>
2023-11-17 17:11:39 +0300

    Add RootLayout: add component

--- components/RootLayout/RootLayout.jsx
+++ components/RootLayout/RootLayout.jsx
@@ -1,4 +1,17 @@
 import Head from 'next/head';
+import cn from 'classnames';
+
+import { Roboto } from 'next/font/google';
+
+import Navigation from '../Navigation/Navigation';
+
+import s from './RootLayout.module.scss';
+
+const font = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function RootLayout({ children }) {
   return (
@@ -9,7 +22,10 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>{children}</main>
+      <main className={cn(s.main, font.className)}>
+        <Navigation />
+        {children}
+      </main>
     </>
   );
 }
