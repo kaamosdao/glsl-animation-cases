9fa3a572b309d8102085e7c4af8c5eb4f602e3bc
Alexander <kaamosdao@gmail.com>
2023-12-01 16:16:28 +0300

    Change app: ad letters to home page

--- pages/index.js
+++ pages/index.js
@@ -48,6 +48,14 @@ export default function Home() {
 
   return (
     <div ref={canvasHolder} className={s.canvasHolder}>
+      <div className={s.container}>
+        <p className={s.letter}>k</p>
+        <p className={s.letter}>a</p>
+        <p className={s.letter}>a</p>
+        <p className={s.letter}>m</p>
+        <p className={s.letter}>o</p>
+        <p className={s.letter}>s</p>
+      </div>
       <canvas ref={canvas} />
     </div>
   );
